d2b8525fd16d382db684dc15ee68a4d3a7074b8b
preke <geek00021@gmail.com>
2018-04-12 17:49:02 +0800

    .

--- codes/cnn_online_forum/model.py
+++ codes/cnn_online_forum/model.py
@@ -32,8 +32,8 @@ class CNN_Text(nn.Module):
     def forward(self, q1, q2):
 
         q1 = self.embed(q1)
-        if self.args.static:
-            q1 = Variable(q1)
+        # if self.args.static:
+        #     q1 = Variable(q1)
         q1 = q1.unsqueeze(1)  # (N, Ci, W, D)
         q1 = [F.tanh(conv(q1)).squeeze(3) for conv in self.convs1]  # [(N, Co, W), ...]*len(Ks)
         q1 = [i.size(2) * F.avg_pool1d(i, i.size(2)).squeeze(2) for i in q1]  # [(N, Co), ...]*len(Ks)
@@ -41,8 +41,8 @@ class CNN_Text(nn.Module):
         q1 = torch.cat(q1, 1) # 64 * 300
         
         q2 = self.embed(q2)
-        if self.args.static:
-            q2 = Variable(q2)
+        # if self.args.static:
+        #     q2 = Variable(q2)
         q2 = q2.unsqueeze(1)  # (N, Ci, W, D)
         q2 = [F.tanh(conv(q2)).squeeze(3) for conv in self.convs1]  # [(N, Co, W), ...]*len(Ks)
         q2 = [i.size(2) * F.avg_pool1d(i, i.size(2)).squeeze(2) for i in q2]  # [(N, Co), ...]*len(Ks)
