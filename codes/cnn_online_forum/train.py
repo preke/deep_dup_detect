fa6a43f40aa941a6591eb30a11098e293b2a64c0
preke <geek00021@gmail.com>
2018-04-12 20:02:59 +0800

    .

--- codes/cnn_online_forum/train.py
+++ codes/cnn_online_forum/train.py
@@ -91,9 +91,9 @@ def eval(data_iter, model, args):
         for i in range(length):
             a = logit.data[i]
             b = target.data[i]
-            if a < 0.5 and b == 0:
+            if a < 0.1 and b == 0:
                 corrects += 1
-            elif a >= 0.5 and b == 1:
+            elif a >= 0.1 and b == 1:
                 corrects += 1
             else:
                 pass
