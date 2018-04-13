ff5706129637b7cff07fab51cd8126b929337c5d
preke <geek00021@gmail.com>
2018-04-13 16:21:31 +0800

    .

--- codes/cnn_online_forum/load_data.py
+++ codes/cnn_online_forum/load_data.py
@@ -47,7 +47,7 @@ def gen_iter(path, text_field, label_field, pair_id_field, args):
     tmp_iter = data.BucketIterator(
                     tmp_data,
                     batch_size=args.batch_size,
-                    sort_key=lambda x: len(x),
+                    sort_key=lambda x: len(x.question1) + len(x.question2),
                     device=0,
                     repeat=False)
     return tmp_data, tmp_iter
