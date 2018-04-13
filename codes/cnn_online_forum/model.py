66bd24b36722e865d1e56730cbddc162b54c9e37
preke <geek00021@gmail.com>
2018-04-13 11:01:00 +0800

    .

--- codes/cnn_online_forum/model.py
+++ codes/cnn_online_forum/model.py
@@ -79,6 +79,7 @@ class CNN_Sim(nn.Module):
         q1 = cnn1.forward(q1)
         q2 = cnn2.forward(q2)
         cos_ans = F.cosine_similarity(q1, q2)
+        print(type(cos_ans))
         return cos_ans
 
 
